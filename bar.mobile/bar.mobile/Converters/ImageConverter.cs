8b52d6cfdb1b381cf3834a06c0691dec17d7607e
Jack <jack@nicholssoftware.com>
2019-12-26 10:12:09 -0500

    added an image converter

---
+++ bar.mobile/bar.mobile/Converters/ImageConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace bar.mobile.Converters
+{
+    public class ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(ImageSource))
+            {
+                throw new InvalidOperationException("Invalid target type.");
+            }
+
+            try
+            {
+                return new Uri("https://s3-media0.fl.yelpcdn.com/bphoto/-qYFoL7TuHTZ0PQjfREeAw/ls.jpg");
+            }
+            catch(Exception ex)
+            {
+                return null; 
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
