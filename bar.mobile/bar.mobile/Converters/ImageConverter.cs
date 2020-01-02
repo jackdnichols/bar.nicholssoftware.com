dfa04bee61727f6605fabcba7a112e537803fe8b
Jack <jack@nicholssoftware.com>
2020-01-02 08:49:55 -0500

    Xamarin fix for remote images

--- bar.mobile/bar.mobile/Converters/ImageConverter.cs
+++ bar.mobile/bar.mobile/Converters/ImageConverter.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using System.Globalization;
+using System.Reflection;
 using Xamarin.Forms;
+using System.Drawing;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace bar.mobile.Converters
 {
@@ -15,7 +22,16 @@ namespace bar.mobile.Converters
 
             try
             {
-                return ImageSource.FromUri(new Uri(value.ToString()));
+                /*
+                    Image image = new Image();
+                    Stream stream = new MemoryStream(byteArray);
+                    image.Source = ImageSource.FromStream(stream);
+                */
+                Uri uri = new Uri(value.ToString());
+                ImageSource imgSource = UriImageSource.FromUri(uri);
+                return imgSource;
+                //return ImageSource.FromUri(new Uri(value.ToString(), UriKind.RelativeOrAbsolute));
+                //return GetImageStreamFromURL(value.ToString());
             }
             catch(Exception ex)
             {
@@ -26,6 +42,19 @@ namespace bar.mobile.Converters
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
+        }
+
+        private async Task<Xamarin.Forms.Image> GetImageStreamFromURL(String imageUrl)
+        {
+            HttpClient client = new HttpClient();
+            client.MaxResponseContentBufferSize = 256000;
+            //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            Stream stream = await client.GetStreamAsync(imageUrl);
+
+            Xamarin.Forms.Image img = new Xamarin.Forms.Image();
+            img.Source = ImageSource.FromStream(() => stream);
+
+            return img;
         }
     }
 }
---
+++ bar.mobile/bar.mobile/System/Drawing/Image.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Xamarin.Forms;
+
+namespace System.Drawing
+{
+    internal class Image
+    {
+        internal ImageSource Source;
+
+        internal static Image FromStream(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
